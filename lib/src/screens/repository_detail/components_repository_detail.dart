1d64e4bfbda1bd4566a627679363e14dad9c4091
Simone Stasi <simone.stasi@purplesoft.io>
2023-02-28 18:03:26 +0100

    repos: switch branch only in root directory

--- lib/src/screens/repository_detail/components_repository_detail.dart
+++ lib/src/screens/repository_detail/components_repository_detail.dart
@@ -1,1 +1,49 @@
 part of repository_detail;
+
+class _BranchRow extends StatelessWidget {
+  const _BranchRow({required this.ctrl});
+
+  final _RepositoryDetailController ctrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        FilterMenu(
+          title: 'Branch',
+          values: ctrl.branches,
+          currentFilter: ctrl.currentBranch,
+          onSelected: ctrl.changeBranch,
+          formatLabel: (b) => '${b!.name} ${b.isBaseVersion ? '(default)' : ''}',
+          isDefaultFilter: false,
+        ),
+        const Spacer(),
+        if (ctrl.currentBranch != null && ctrl.currentBranch!.behindCount > 0) ...[
+          Icon(
+            Icons.remove,
+            size: 12,
+            color: Colors.red,
+          ),
+          Text(
+            ctrl.currentBranch!.behindCount.toString(),
+            style: context.textTheme.titleSmall!.copyWith(color: Colors.red),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+        ],
+        if (ctrl.currentBranch != null && ctrl.currentBranch!.aheadCount > 0) ...[
+          Icon(
+            DevOpsIcons.plus,
+            size: 12,
+            color: Colors.green,
+          ),
+          Text(
+            ctrl.currentBranch!.aheadCount.toString(),
+            style: context.textTheme.titleSmall!.copyWith(color: Colors.green),
+          ),
+        ],
+      ],
+    );
+  }
+}
