65a936681d99fbcc4d88a027746e2049bedfaeb2
Simone Stasi <simone.stasi@purplesoft.io>
2023-05-05 16:54:51 +0200

    work_items: show images full screen on tap

--- lib/src/extensions/context_extension.dart
+++ lib/src/extensions/context_extension.dart
@@ -4,4 +4,5 @@ extension PurpleContext on BuildContext {
   TextTheme get textTheme => Theme.of(this).textTheme;
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
   double get height => MediaQuery.of(this).size.height;
+  double get width => MediaQuery.of(this).size.width;
 }
--- lib/src/screens/work_item_detail/components_work_item_detail.dart
+++ lib/src/screens/work_item_detail/components_work_item_detail.dart
@@ -19,15 +19,50 @@ class _HtmlWidget extends StatelessWidget {
       },
       customRenders: {
         (ctx) => ctx.tree.element?.localName == 'img': CustomRender.widget(
-          widget: (ctx, child) => Image.network(
-            ctx.tree.attributes['src']!,
-            headers: apiService.headers,
-            fit: BoxFit.contain,
-            height: double.tryParse(ctx.tree.attributes['height'] ?? ''),
-            width: double.tryParse(ctx.tree.attributes['width'] ?? ''),
-            frameBuilder: (_, child, frame, __) =>
-                frame == null ? Center(child: const CircularProgressIndicator()) : child,
-          ),
+          widget: (ctx, child) {
+            final image = CachedNetworkImage(
+              imageUrl: ctx.tree.attributes['src']!,
+              httpHeaders: apiService.headers,
+              fit: BoxFit.contain,
+              height: double.tryParse(ctx.tree.attributes['height'] ?? ''),
+              width: double.tryParse(ctx.tree.attributes['width'] ?? ''),
+              placeholder: (_, __) => Center(child: const CircularProgressIndicator()),
+            );
+
+            late OverlayEntry entry;
+
+            void exitFullScreen() {
+              entry.remove();
+            }
+
+            void goFullScreen() {
+              entry = OverlayEntry(
+                builder: (context) => Scaffold(
+                  appBar: AppBar(
+                    actions: [
+                      CloseButton(
+                        onPressed: exitFullScreen,
+                      ),
+                    ],
+                  ),
+                  body: InteractiveViewer(
+                    child: SizedBox(
+                      height: context.height,
+                      width: context.width,
+                      child: image,
+                    ),
+                  ),
+                ),
+              );
+
+              Overlay.of(context).insert(entry);
+            }
+
+            return GestureDetector(
+              onTap: goFullScreen,
+              child: image,
+            );
+          },
         ),
         (ctx) => ctx.tree.element?.localName == 'br': CustomRender.widget(
           widget: (ctx, child) => const Text('\n'),
