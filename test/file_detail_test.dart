45b53e6ce6193938c0ff1f96a7d7afd3cce7ca9f
Alberto Furlan <alberto.furlan@purplesoft.io>
2023-12-07 11:20:15 +0100

    removed unused dependencies

--- test/file_detail_test.dart
+++ test/file_detail_test.dart
@@ -2,7 +2,6 @@ import 'package:azure_devops/src/router/router.dart';
 import 'package:azure_devops/src/screens/file_detail/base_file_detail.dart';
 import 'package:azure_devops/src/services/azure_api_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'api_service_mock.dart';
--- test/member_detail_test.dart
+++ test/member_detail_test.dart
@@ -1,9 +1,5 @@
-import 'dart:developer';
-
 import 'package:azure_devops/src/screens/member_detail/base_member_detail.dart';
 import 'package:azure_devops/src/services/azure_api_service.dart';
-import 'package:azure_devops/src/theme/dev_ops_icons_icons.dart';
-import 'package:azure_devops/src/widgets/text_title_description.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
