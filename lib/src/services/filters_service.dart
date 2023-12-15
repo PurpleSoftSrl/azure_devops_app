cf417881b8aafd2551b88e54c35f4ae40d5b8b4f
Simone Stasi <simone.stasi@purplesoft.io>
2023-12-15 16:34:33 +0100

    filters_service: move filter keys in their own classes

--- lib/src/services/filters_service.dart
+++ lib/src/services/filters_service.dart
@@ -21,37 +21,37 @@ class FiltersService {
         .toList();
 
     return WorkItemsFilters(
-      projects: workItemsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.projects)?.filters ?? {},
-      states: workItemsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.states)?.filters ?? {},
-      types: workItemsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.types)?.filters ?? {},
-      assignees: workItemsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.assignees)?.filters ?? {},
-      area: workItemsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.area)?.filters ?? {},
-      iteration: workItemsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.iteration)?.filters ?? {},
+      projects: workItemsFilters.firstWhereOrNull((f) => f.attribute == WorkItemsFilters.projectsKey)?.filters ?? {},
+      states: workItemsFilters.firstWhereOrNull((f) => f.attribute == WorkItemsFilters.statesKey)?.filters ?? {},
+      types: workItemsFilters.firstWhereOrNull((f) => f.attribute == WorkItemsFilters.typesKey)?.filters ?? {},
+      assignees: workItemsFilters.firstWhereOrNull((f) => f.attribute == WorkItemsFilters.assigneesKey)?.filters ?? {},
+      area: workItemsFilters.firstWhereOrNull((f) => f.attribute == WorkItemsFilters.areaKey)?.filters ?? {},
+      iteration: workItemsFilters.firstWhereOrNull((f) => f.attribute == WorkItemsFilters.iterationKey)?.filters ?? {},
     );
   }
 
   void saveWorkItemsProjectsFilter(Set<String> projectNames) {
-    storageService.saveFilter(organization, _FilterAreas.workItems, _FilterKeys.projects, projectNames);
+    storageService.saveFilter(organization, _FilterAreas.workItems, WorkItemsFilters.projectsKey, projectNames);
   }
 
   void saveWorkItemsStatesFilter(Set<String> stateNames) {
-    storageService.saveFilter(organization, _FilterAreas.workItems, _FilterKeys.states, stateNames);
+    storageService.saveFilter(organization, _FilterAreas.workItems, WorkItemsFilters.statesKey, stateNames);
   }
 
   void saveWorkItemsTypesFilter(Set<String> typeNames) {
-    storageService.saveFilter(organization, _FilterAreas.workItems, _FilterKeys.types, typeNames);
+    storageService.saveFilter(organization, _FilterAreas.workItems, WorkItemsFilters.typesKey, typeNames);
   }
 
   void saveWorkItemsAssigneesFilter(Set<String> userEmails) {
-    storageService.saveFilter(organization, _FilterAreas.workItems, _FilterKeys.assignees, userEmails);
+    storageService.saveFilter(organization, _FilterAreas.workItems, WorkItemsFilters.assigneesKey, userEmails);
   }
 
   void saveWorkItemsAreaFilter(String area) {
-    storageService.saveFilter(organization, _FilterAreas.workItems, _FilterKeys.area, {area});
+    storageService.saveFilter(organization, _FilterAreas.workItems, WorkItemsFilters.areaKey, {area});
   }
 
   void saveWorkItemsIterationFilter(String iteration) {
-    storageService.saveFilter(organization, _FilterAreas.workItems, _FilterKeys.iteration, {iteration});
+    storageService.saveFilter(organization, _FilterAreas.workItems, WorkItemsFilters.iterationKey, {iteration});
   }
 
   void resetWorkItemsFilters() {
@@ -68,17 +68,17 @@ class FiltersService {
         .toList();
 
     return CommitsFilters(
-      projects: commitsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.projects)?.filters ?? {},
-      authors: commitsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.authors)?.filters ?? {},
+      projects: commitsFilters.firstWhereOrNull((f) => f.attribute == CommitsFilters.projectsKey)?.filters ?? {},
+      authors: commitsFilters.firstWhereOrNull((f) => f.attribute == CommitsFilters.authorsKey)?.filters ?? {},
     );
   }
 
   void saveCommitsProjectsFilter(Set<String> projectNames) {
-    storageService.saveFilter(organization, _FilterAreas.commits, _FilterKeys.projects, projectNames);
+    storageService.saveFilter(organization, _FilterAreas.commits, CommitsFilters.projectsKey, projectNames);
   }
 
   void saveCommitsAuthorsFilter(Set<String> userEmails) {
-    storageService.saveFilter(organization, _FilterAreas.commits, _FilterKeys.authors, userEmails);
+    storageService.saveFilter(organization, _FilterAreas.commits, CommitsFilters.authorsKey, userEmails);
   }
 
   void resetCommitsFilters() {
@@ -95,27 +95,28 @@ class FiltersService {
         .toList();
 
     return PipelinesFilters(
-      projects: pipelinesFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.projects)?.filters ?? {},
-      triggeredBy: pipelinesFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.triggeredBy)?.filters ?? {},
-      result: pipelinesFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.result)?.filters ?? {},
-      status: pipelinesFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.status)?.filters ?? {},
+      projects: pipelinesFilters.firstWhereOrNull((f) => f.attribute == PipelinesFilters.projectsKey)?.filters ?? {},
+      triggeredBy:
+          pipelinesFilters.firstWhereOrNull((f) => f.attribute == PipelinesFilters.triggeredByKey)?.filters ?? {},
+      result: pipelinesFilters.firstWhereOrNull((f) => f.attribute == PipelinesFilters.resultKey)?.filters ?? {},
+      status: pipelinesFilters.firstWhereOrNull((f) => f.attribute == PipelinesFilters.statusKey)?.filters ?? {},
     );
   }
 
   void savePipelinesProjectsFilter(Set<String> projectNames) {
-    storageService.saveFilter(organization, _FilterAreas.pipelines, _FilterKeys.projects, projectNames);
+    storageService.saveFilter(organization, _FilterAreas.pipelines, PipelinesFilters.projectsKey, projectNames);
   }
 
   void savePipelinesTriggeredByFilter(Set<String> userEmails) {
-    storageService.saveFilter(organization, _FilterAreas.pipelines, _FilterKeys.triggeredBy, userEmails);
+    storageService.saveFilter(organization, _FilterAreas.pipelines, PipelinesFilters.triggeredByKey, userEmails);
   }
 
   void savePipelinesResultFilter(String result) {
-    storageService.saveFilter(organization, _FilterAreas.pipelines, _FilterKeys.result, {result});
+    storageService.saveFilter(organization, _FilterAreas.pipelines, PipelinesFilters.resultKey, {result});
   }
 
   void savePipelinesStatusFilter(String status) {
-    storageService.saveFilter(organization, _FilterAreas.pipelines, _FilterKeys.status, {status});
+    storageService.saveFilter(organization, _FilterAreas.pipelines, PipelinesFilters.statusKey, {status});
   }
 
   void resetPipelinesFilters() {
@@ -132,27 +133,30 @@ class FiltersService {
         .toList();
 
     return PullRequestsFilters(
-      projects: pullRequestsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.projects)?.filters ?? {},
-      status: pullRequestsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.status)?.filters ?? {},
-      openedBy: pullRequestsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.openedBy)?.filters ?? {},
-      assignedTo: pullRequestsFilters.firstWhereOrNull((f) => f.attribute == _FilterKeys.assignees)?.filters ?? {},
+      projects:
+          pullRequestsFilters.firstWhereOrNull((f) => f.attribute == PullRequestsFilters.projectsKey)?.filters ?? {},
+      status: pullRequestsFilters.firstWhereOrNull((f) => f.attribute == PullRequestsFilters.statusKey)?.filters ?? {},
+      openedBy:
+          pullRequestsFilters.firstWhereOrNull((f) => f.attribute == PullRequestsFilters.openedByKey)?.filters ?? {},
+      assignedTo:
+          pullRequestsFilters.firstWhereOrNull((f) => f.attribute == PullRequestsFilters.assignedToKey)?.filters ?? {},
     );
   }
 
   void savePullRequestsProjectsFilter(Set<String> projectNames) {
-    storageService.saveFilter(organization, _FilterAreas.pullRequests, _FilterKeys.projects, projectNames);
+    storageService.saveFilter(organization, _FilterAreas.pullRequests, PullRequestsFilters.projectsKey, projectNames);
   }
 
   void savePullRequestsStatusFilter(String status) {
-    storageService.saveFilter(organization, _FilterAreas.pullRequests, _FilterKeys.status, {status});
+    storageService.saveFilter(organization, _FilterAreas.pullRequests, PullRequestsFilters.statusKey, {status});
   }
 
   void savePullRequestsOpenedByFilter(Set<String> userEmails) {
-    storageService.saveFilter(organization, _FilterAreas.pullRequests, _FilterKeys.openedBy, userEmails);
+    storageService.saveFilter(organization, _FilterAreas.pullRequests, PullRequestsFilters.openedByKey, userEmails);
   }
 
   void savePullRequestsAssignedToFilter(Set<String> userEmails) {
-    storageService.saveFilter(organization, _FilterAreas.pullRequests, _FilterKeys.assignees, userEmails);
+    storageService.saveFilter(organization, _FilterAreas.pullRequests, PullRequestsFilters.assignedToKey, userEmails);
   }
 
   void resetPullRequestsFilters() {
@@ -170,6 +174,13 @@ class WorkItemsFilters {
     required this.iteration,
   });
 
+  static const projectsKey = 'projects';
+  static const statesKey = 'states';
+  static const typesKey = 'types';
+  static const assigneesKey = 'assignees';
+  static const areaKey = 'area';
+  static const iterationKey = 'iteration';
+
   final Set<String> projects;
   final Set<String> states;
   final Set<String> types;
@@ -184,6 +195,9 @@ class CommitsFilters {
     required this.authors,
   });
 
+  static const projectsKey = 'projects';
+  static const authorsKey = 'authors';
+
   final Set<String> projects;
   final Set<String> authors;
 }
@@ -195,6 +209,11 @@ class PipelinesFilters {
     required this.result,
     required this.status,
   });
+
+  static const projectsKey = 'projects';
+  static const triggeredByKey = 'triggeredBy';
+  static const resultKey = 'result';
+  static const statusKey = 'status';
 
   final Set<String> projects;
   final Set<String> triggeredBy;
@@ -210,6 +229,11 @@ class PullRequestsFilters {
     required this.assignedTo,
   });
 
+  static const projectsKey = 'projects';
+  static const statusKey = 'status';
+  static const openedByKey = 'openedBy';
+  static const assignedToKey = 'assignedTo';
+
   final Set<String> projects;
   final Set<String> status;
   final Set<String> openedBy;
@@ -221,19 +245,4 @@ class _FilterAreas {
   static const commits = 'commits';
   static const pipelines = 'pipelines';
   static const pullRequests = 'pull-requests';
-}
-
-// TODO move keys in each area class
-class _FilterKeys {
-  static const projects = 'projects';
-  static const states = 'states';
-  static const types = 'types';
-  static const assignees = 'assignees';
-  static const area = 'area';
-  static const iteration = 'iteration';
-  static const authors = 'authors';
-  static const triggeredBy = 'triggeredBy';
-  static const result = 'result';
-  static const status = 'status';
-  static const openedBy = 'openedBy';
 }
