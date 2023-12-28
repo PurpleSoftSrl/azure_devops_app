b87348a588dc8bb1f24c6bcd6f649e86bd028d56
Alberto Furlan <alberto.furlan@purplesoft.io>
2023-12-28 10:21:43 +0100

    work items filters: merged conditions of different checks.

--- lib/src/screens/work_items/controller_work_items.dart
+++ lib/src/screens/work_items/controller_work_items.dart
@@ -112,10 +112,8 @@ class _WorkItemsController with FilterMixin {
   WorkItemsFilters _fillSavedFilters() {
     final savedFilters = filtersService.getWorkItemsSavedFilters();
 
-    if (savedFilters.projects.isNotEmpty) {
-      if (project == null) {
-        projectsFilter = getProjects(storageService).where((p) => savedFilters.projects.contains(p.name)).toSet();
-      }
+    if (savedFilters.projects.isNotEmpty && project == null) {
+      projectsFilter = getProjects(storageService).where((p) => savedFilters.projects.contains(p.name)).toSet();
     }
 
     if ((project != null && savedFilters.projects.contains(project!.name)) || project == null) {
