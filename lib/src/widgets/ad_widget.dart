7071b284c0f33b0bb57174d9e79798c0dd132a2d
Simone Stasi <simone.stasi@purplesoft.io>
2025-04-29 17:24:22 +0200

    improve UI

--- lib/src/widgets/ad_widget.dart
+++ lib/src/widgets/ad_widget.dart
@@ -54,72 +54,69 @@ class AmazonAdWidget extends StatelessWidget {
     final hasDiscount = item.discount != null && item.discount!.amount > 0;
     return SizedBox(
       height: 160,
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 12),
-        child: Center(
-          child: GestureDetector(
-            onTap: () => launchUrlString(item.itemUrl, mode: LaunchMode.externalApplication),
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(4),
-                  child: Image.network(
-                    item.imageUrl,
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
+      child: Center(
+        child: GestureDetector(
+          onTap: () => launchUrlString(item.itemUrl, mode: LaunchMode.externalApplication),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Image.network(
+                  item.imageUrl,
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        item.title,
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: context.textTheme.titleSmall,
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          if (hasDiscount) ...[
-                            Text(
-                              '-${item.discount!.percentage.toPercentage()}',
-                              style: context.textTheme.bodyMedium!.copyWith(
-                                color: context.colorScheme.error,
-                                fontWeight: FontWeight.normal,
-                                fontFamily: AppTheme.defaultFont,
-                              ),
-                            ),
-                            const SizedBox(width: 4),
-                            Text(
-                              item.discountedPrice.amount.toCurrency(item.currency),
-                              style: context.textTheme.bodyMedium,
-                            ),
-                            const SizedBox(width: 8),
-                          ],
+              ),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      item.title,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: context.textTheme.titleSmall,
+                    ),
+                    const SizedBox(height: 8),
+                    Row(
+                      children: [
+                        if (hasDiscount) ...[
                           Text(
-                            item.originalPrice.amount.toCurrency(item.currency),
-                            style: context.textTheme.bodySmall?.copyWith(
-                              decoration: hasDiscount ? TextDecoration.lineThrough : null,
+                            '-${item.discount!.percentage.toPercentage()}',
+                            style: context.textTheme.bodyMedium!.copyWith(
+                              color: context.colorScheme.error,
+                              fontWeight: FontWeight.normal,
+                              fontFamily: AppTheme.defaultFont,
                             ),
                           ),
-                          if (item.isPrime) ...[
-                            const Spacer(),
-                            SvgPicture.network(
-                              'https://m.media-amazon.com/images/G/29/perc/prime-logo.png',
-                              width: 45,
-                            ),
-                          ],
+                          const SizedBox(width: 4),
+                          Text(
+                            item.discountedPrice.amount.toCurrency(item.currency),
+                            style: context.textTheme.bodyMedium,
+                          ),
+                          const SizedBox(width: 8),
                         ],
-                      ),
-                    ],
-                  ),
+                        Text(
+                          item.originalPrice.amount.toCurrency(item.currency),
+                          style: context.textTheme.bodySmall?.copyWith(
+                            decoration: hasDiscount ? TextDecoration.lineThrough : null,
+                          ),
+                        ),
+                        if (item.isPrime) ...[
+                          const Spacer(),
+                          SvgPicture.network(
+                            'https://m.media-amazon.com/images/G/29/perc/prime-logo.png',
+                            width: 45,
+                          ),
+                        ],
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/src/widgets/pipeline_list_tile.dart
+++ lib/src/widgets/pipeline_list_tile.dart
@@ -27,75 +27,78 @@ class PipelineListTile extends StatelessWidget {
       key: ValueKey('pipeline_${pipe.id}'),
       child: Column(
         children: [
-          Row(
-            children: [
-              if (pipe.status == PipelineStatus.inProgress && pipe.approvals.isNotEmpty)
-                Icon(
-                  Icons.warning,
-                  color: Colors.orange,
-                )
-              else
-                pipe.status == PipelineStatus.completed ? pipe.result.icon : pipe.status.icon,
-              const SizedBox(
-                width: 12,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      pipe.triggerInfo?.ciMessage ?? pipe.reason ?? '',
-                      style: context.textTheme.labelLarge,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          pipe.requestedFor?.displayName ?? '',
-                          style: subtitleStyle,
-                        ),
-                        Text(
-                          ' in ',
-                          style: subtitleStyle.copyWith(color: context.colorScheme.onSecondary),
-                        ),
-                        Expanded(
-                          child: Text(
-                            pipe.repository?.name ?? '-',
-                            overflow: TextOverflow.ellipsis,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            child: Row(
+              children: [
+                if (pipe.status == PipelineStatus.inProgress && pipe.approvals.isNotEmpty)
+                  Icon(
+                    Icons.warning,
+                    color: Colors.orange,
+                  )
+                else
+                  pipe.status == PipelineStatus.completed ? pipe.result.icon : pipe.status.icon,
+                const SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        pipe.triggerInfo?.ciMessage ?? pipe.reason ?? '',
+                        style: context.textTheme.labelLarge,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            pipe.requestedFor?.displayName ?? '',
                             style: subtitleStyle,
                           ),
+                          Text(
+                            ' in ',
+                            style: subtitleStyle.copyWith(color: context.colorScheme.onSecondary),
+                          ),
+                          Expanded(
+                            child: Text(
+                              pipe.repository?.name ?? '-',
+                              overflow: TextOverflow.ellipsis,
+                              style: subtitleStyle,
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (isCustomPipelineName) ...[
+                        const SizedBox(
+                          height: 3,
+                        ),
+                        Text(
+                          pipe.definition!.name!,
+                          style: subtitleStyle,
                         ),
                       ],
-                    ),
-                    if (isCustomPipelineName) ...[
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      Text(
-                        pipe.definition!.name!,
-                        style: subtitleStyle,
-                      ),
                     ],
-                  ],
+                  ),
                 ),
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              Text(
-                pipe.startTime?.minutesAgo ?? '',
-                style: subtitleStyle,
-              ),
-            ],
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(
+                  pipe.startTime?.minutesAgo ?? '',
+                  style: subtitleStyle,
+                ),
+              ],
+            ),
           ),
           if (!isLast)
             AppLayoutBuilder(
-              smartphone: Divider(height: 24, thickness: 1),
-              tablet: Divider(height: 48, thickness: 1),
+              smartphone: const Divider(height: 1, thickness: 1),
+              tablet: const Divider(height: 10, thickness: 1),
             ),
         ],
       ),
