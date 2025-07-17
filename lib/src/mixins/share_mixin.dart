2abffe21b25ea95f42fcb4d904c907ef039e87b6
Simone Stasi <simone.stasi@purplesoft.io>
2025-07-17 16:50:23 +0200

    upgrade dependencies

--- lib/src/mixins/share_mixin.dart
+++ lib/src/mixins/share_mixin.dart
@@ -6,12 +6,14 @@ import 'package:share_plus/share_plus.dart';
 mixin ShareMixin {
   void shareUrl(String url) {
     final size = MediaQueryData.fromView(PlatformDispatcher.instance.views.first).size;
-    Share.share(
-      url,
-      sharePositionOrigin: Rect.fromCenter(
-        center: Offset(size.width / 2, size.height / 2),
-        width: size.width / 2,
-        height: size.height / 2,
+    SharePlus.instance.share(
+      ShareParams(
+        uri: Uri.parse(url),
+        sharePositionOrigin: Rect.fromCenter(
+          center: Offset(size.width / 2, size.height / 2),
+          width: size.width / 2,
+          height: size.height / 2,
+        ),
       ),
     );
   }
